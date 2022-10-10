c58d8480ff2800cf01012192a25e59541210a7f6
SondosSaber <sondossaber2495@gmail.com>
2022-10-10 21:54:22 +0200

    add login Test Case

--- EcommerceApp/src/test/java/BaseTest.java
+++ EcommerceApp/src/test/java/BaseTest.java
@@ -1,15 +1,23 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
-    public static void main(String[] args) {
-        //set driver path
-
+    public static void main(String[] args) throws InterruptedException {
+        //set driver path - set property
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
 
         //new object of webdriver
         WebDriver driver = new ChromeDriver();
 
-        //actions
         driver.navigate().to("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        Thread.sleep(3000);
+        driver.close();
     }
 }
