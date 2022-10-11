30c0c518a1c6859ced1244148dfb7251ad718f19
SondosSaber <sondossaber2495@gmail.com>
2022-10-11 09:53:28 +0200

    add tcs

--- EcommerceApp/src/test/java/SC12.java
+++ EcommerceApp/src/test/java/SC12.java
@@ -39,14 +39,14 @@ public class SC12 {
 //        driver.findElement(By.xpath("//input[@id='BillingNewAddress_PhoneNumber']")).sendKeys("123456789");
 //        Thread.sleep(6000);
         driver.findElement(By.xpath("//div[@id='billing-buttons-container']//button[@class='button-1 new-address-next-step-button' and text()='Continue']")).click();
-        Thread.sleep(6000);
-        driver.findElement(By.xpath("//div[@id='billing-buttons-container']//button[@class='button-1 new-address-next-step-button' and text()='Continue']")).click();
-        Thread.sleep(6000);
-        driver.findElement(By.xpath("//div[@id='billing-buttons-container']//button[@class='button-1 new-address-next-step-button' and text()='Continue']")).click();
-        Thread.sleep(6000);
-        driver.findElement(By.xpath("//div[@id='billing-buttons-container']//button[@class='button-1 new-address-next-step-button' and text()='Continue']")).click();
-        Thread.sleep(6000);
-        driver.findElement(By.xpath("//div[@id='billing-buttons-container']//button[@class='button-1 new-address-next-step-button' and text()='Continue']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@class='button-1 shipping-method-next-step-button']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@class='button-1 payment-method-next-step-button']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@class='button-1 payment-info-next-step-button']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@class='button-1 confirm-order-next-step-button']")).click();
 
         //Asset
 //        driver.findElement(By.xpath("//div[@class='title']/strong[text()='Your order has been successfully processed!']")).
